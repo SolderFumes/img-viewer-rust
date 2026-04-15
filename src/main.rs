9cf2bf3e6119e2416c51a50f11bc08cd2490034a
solderfumes <luka.schuller@gmail.com>
2026-04-15 14:32:48 -0700

    Files can be loaded!

--- src/main.rs
+++ src/main.rs
@@ -7,3 +7,13 @@
 // 3) PAINT WINDOW 
 // 4) ???
 // 5) PROFIT
+//
+mod read_bmp;
+
+fn main() {
+    let res = read_bmp::read_bmp("/home/solderfumes/personal/img-viewer-rust/1bpp-320x240.bmp");
+    if res.is_err() {
+        println!("What happpened??");
+        let _ = dbg!(res);
+    }
+}
--- src/read_bmp.rs
+++ src/read_bmp.rs
@@ -1,24 +1,26 @@
-// Read a Bitmap image as a Byte array. 
-// How do we do this?
-// There is a function file_var.read(&mut buffer[..]) which returns a Result<usize> (usize is a mem address/ptr). The result (when unwrapped) tells you how many bytes were used.
-// So we can read bytes up until we've filled the buffer, then store that data in a Vec<u8> (more
-// permanent collection) then read from (the start of the file + n) until we fill up the buffer
-// and repeat :3.
-//
-//
-// Turns out there's a better way. Since there's a file size in bytes in the BMP header, we don't
-// even have to use a buffer except for the header. We read the header (54 bytes) into a buffer,
-// find the total length, then make a Vec<u8> with that many bytes minus 54. Now, we can read
-// directly into that Vec<u8> without having to use any buffer since we know the exact length.
-//
-// We do need to keep the entire file in memory.
-//
 use std::fs::File;
 use std::io::prelude::*;
 
-fn read_bmp(target_file: &str) -> () { 
-    let mut file = File::open(target_file)?; //File::open returns a Result<()> so we use ?
+pub fn read_bmp(target_file: &str) -> Result<Vec<u8>, &'static str> { 
+    let file_res = File::open(target_file); //File::open returns a Result<()> so we use ?
                                              //to unpack the actual return value. 
-    let mut buf = [52]
+    let mut file: File;
+    if file_res.is_err() {
+        return Err("File opening failed.");
+    } else {
+        file = file_res.unwrap()
+    }
+    let mut header: [u8; 52] = [0; 52];
+    let res = file.read_exact(&mut header); // This will read until buf is full.
+    if res.is_err() {
+        return Err("File couldn't be read");
+    }
+
+    dbg!(header);
+
+    let img_vec: Vec<u8> = Vec::from(header);
+
+    return Ok(img_vec);
+
 
 }
